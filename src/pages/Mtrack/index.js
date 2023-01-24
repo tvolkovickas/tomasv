fe046dfab891316d4672af0e18b6929f52e60499
tvolkovickas <tvolkovickas@gmail.com>
2023-01-24 22:55:48 +0000

    Add timer

--- src/pages/Mtrack/index.js
+++ src/pages/Mtrack/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import cn from "classnames";
 
 import styles from "./style.module.css";
 
 const Mtrack = () => {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [counter, setCounter] = useState(null);
   const displayItems = [
     "3 mm",
     "Fine Dust",
@@ -39,21 +40,57 @@ const Mtrack = () => {
     if (item === "") {
       return;
     }
+    setCounter(item === selectedItem ? null : 0);
     setSelectedItem(item === selectedItem ? null : item);
   };
+
+  useEffect(() => {
+    let timer1;
+    if (counter !== null) {
+      timer1 = setTimeout(() => {
+        setCounter((counter) => counter + 1);
+      }, 1000);
+    }
+    return () => {
+      clearTimeout(timer1);
+    };
+  }, [counter]);
   return (
-    <div className={styles.container}>
-      {displayItems.map((item, index) => (
-        <div
-          key={index}
-          className={cn(styles.gridItem, {
-            [styles.on]: item === selectedItem,
-          })}
-          onClick={() => toggle(item)}
-        >
-          {item}
+    <div>
+      <div className={styles.container}>
+        {displayItems.map((item, index) => (
+          <div
+            key={index}
+            className={cn(styles.gridItem, {
+              [styles.on]: item === selectedItem,
+            })}
+            onClick={() => toggle(item)}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      {counter !== null && (
+        <div className={styles.counter}>
+          <span>
+            {Math.floor((counter % (60 * 60 * 24)) / (60 * 60))
+              .toString()
+              .padStart(2, "0")}
+          </span>
+          <span className={styles.blinker}>:</span>
+          <span>
+            {Math.floor((counter % (60 * 60)) / 60)
+              .toString()
+              .padStart(2, "0")}
+          </span>
+          <span className={styles.blinker}>:</span>
+          <span>
+            {Math.floor(counter % 60)
+              .toString()
+              .padStart(2, "0")}
+          </span>
         </div>
-      ))}
+      )}
     </div>
   );
 };
